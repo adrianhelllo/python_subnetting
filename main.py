462d4843fbaddfe027e4ca4a2683a529b9dbc32a
adrianhelllo <hetessyadrian@gmail.com>
2025-12-17 18:03:23 +0100

    bug fix

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ def is_valid_network(address):
     network_mask = address[1]
 
     for byte in network_bytes:
-        if any(char.isalpha() for char in byte):
+        if any(not char.isdigit() for char in byte):
             print("The address should only consist of numbers.")
             return False
         if not (0 <= int(byte) <= 255):
             print("The address must only have bytes between 0 and 255.")
             return False
-
         if len(byte) != 1 and byte[0] == 0:
             print("Bytes of the address cannot have leading zeros.")
             return False
@@ -36,10 +35,8 @@ def subnet_info_query(subnet_number):
 def calculate_subnet_info(next_available, users, network):
     subnet_info = {}
     
-
-
 def main():
-    ...
+    network_query()
 
 if __name__ == "__main__":
     main()
