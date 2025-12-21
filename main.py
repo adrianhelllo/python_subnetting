46ac837d09c8da0fa1cbc3012c55ea184ee264d6
adrianhelllo <hetessyadrian@gmail.com>
2025-12-21 15:05:02 +0100

    mask calculations

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ def network_query():
 
     return network_address
 
-def return_int(lst): # Helper
-    return int("".join(lst))
-
 def is_valid_network(cidr):
     try:
         netw_add = ipaddress.IPv4Network(cidr)
@@ -27,10 +24,28 @@ def subnet_info_query():
     info_table = {}
     for i in range(subnets):
         s_name = input(f"Enter the name of subnet {i + 1}: ")
-        s_hosts = int(input("Enter the amount of hosts on subnet {s_name}: "))
+        s_hosts = int(input(f"Enter the amount of hosts on subnet {s_name}: "))
         info_table[s_name] = s_hosts
+
     return info_table
 
+def mask_calculations(hosts, mask):
+    required_bits = math.ceil(math.log2(hosts + 2))
+    mask_bits = mask + required_bits
+    cidr_mask = '/' + str(mask_bits)
+    
+    binary_rep = (mask_bits * '1') + ((32 - mask_bits) * 0)
+    binary_mask = [binary_rep[i:i+8] for i in range(len(binary_rep))]
+
+    decimal_mask = [int(binary_mask[i]) for i in range(len(binary_mask))]
+    return (cidr_mask, binary_mask, decimal_mask)
+
+def calculate_subnet_info(hosts, mask):
+    subnet_info = {}
+    mask_info = mask_calculations(hosts, mask)
+    subnet_info["cidr"] = mask_info[0]
+    subnet_info["b_mask"] = mask_info[1]
+    subnet_info["d_mask"] = mask_info[2]
 
 def calculate_subnet_info(next_available, users, network):
     subnet_info = {}
@@ -38,8 +53,9 @@ def calculate_subnet_info(next_available, users, network):
 def main():
     net = network_query()
     subnet_table = subnet_info_query()
+    print(subnet_table)
 
-    
+
 
 if __name__ == "__main__":
     main()
